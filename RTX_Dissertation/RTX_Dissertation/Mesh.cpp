282b17f4c15dc91df461fc2d3d8df33ca055d5af
Kristian <KristianDixon1206@gmail.com>
2019-07-16 16:59:48 +0100

    Mesh class done for now.

--- RTX_Dissertation/RTX_Dissertation/Mesh.cpp
+++ RTX_Dissertation/RTX_Dissertation/Mesh.cpp
@@ -1,1 +1,19 @@
 #include "Mesh.h"
+Mesh::Mesh(ID3D12ResourcePtr vbo) : mVBO(vbo)
+{
+	//Load BLAS?
+}
+
+int Mesh::AddInstance(mat4& transform)
+{
+	mInstances.push_back(transform);
+	return mInstances.size() - 1;
+}
+
+void Mesh::RemoveInstance(int id)
+{
+	if(id < mInstances.size())
+	{
+		mInstances.erase(mInstances.begin() + id);
+	}
+}
--- RTX_Dissertation/RTX_Dissertation/Mesh.h
+++ RTX_Dissertation/RTX_Dissertation/Mesh.h
@@ -1,13 +1,44 @@
 #pragma once
 #include "Renderer.h"
 
+using namespace glm;
+
 class Mesh
 {
+public:
+	Mesh(ID3D12ResourcePtr vbo);
+
+	//hmm pass by ref?
+	//**********************************************//
+	//Adds an instance to be uploaded into the TLAS
+	//Returns: index of instance entry
+	//**********************************************//
+	int AddInstance(mat4& transform);
+
+	//******************************************//
+	//Removes an instance from the TLAS
+	//id: index of instance to be removed.
+	//******************************************//
+	void RemoveInstance(int id);
+
+	void UpdateInstance(int id, mat4& transform) { mInstances[id] = transform; };
+
+	//******************************************//
+	//Set the pointer to the BLAS buffer
+	//******************************************//
+	void SetBLAS(ID3D12ResourcePtr val) { mBLAS = val; };
+
+	auto GetVBO() { return mVBO; };
+	auto GetBLAS() { return mBLAS; };
+	auto& GetInstances() { return mInstances; };
+private:
 	//Ptr to the VBO
-	ID3D12ResourcePtr mVBO;
+	ID3D12ResourcePtr mVBO = nullptr;
 	//Ptr to the BLAS
-	ID3D12ResourcePtr mBLAS;
-	//List of instances
+	ID3D12ResourcePtr mBLAS = nullptr;
+
+	//List of instances -- TODO: Upgrade to something more flexible in the future such as an instance class.
+	std::vector<mat4> mInstances;
 
 };
 
