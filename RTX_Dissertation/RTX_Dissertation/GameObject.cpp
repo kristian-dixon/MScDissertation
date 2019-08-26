6d06e19e9e80bc2c73de05bba2f4071b6e600041
Kristian <kristiandixon1206@gmail.com>
2019-08-26 16:07:23 +0100

    Switched to a unity friendly json thingy

--- RTX_Dissertation/RTX_Dissertation/GameObject.cpp
+++ RTX_Dissertation/RTX_Dissertation/GameObject.cpp
@@ -13,15 +13,15 @@ void GameObject::LoadFromJson(nlohmann::basic_json<>::value_type& desc, ID3D12Re
 	if(mesh != nullptr)
 	{
 		auto transformArray = desc["Transform"];
-		glm::mat4 transform((float)transformArray.at(0), (float)transformArray.at(1), (float)transformArray.at(2), (float)transformArray.at(3),
+		glm::mat4 transform((float)transformArray[0], (float)transformArray.at(1), (float)transformArray.at(2), (float)transformArray.at(3),
 			(float)transformArray.at(4), (float)transformArray.at(5), (float)transformArray.at(6), (float)transformArray.at(7),
-			(float)transformArray.at(8), (float)transformArray.at(9), (float)transformArray.at(10), (float)transformArray.at(11),
+			(float)transformArray.at(8), (float)transformArray.at(9), (float)transformArray.at(10), (float)transformArray[11],
 			(float)transformArray.at(12), (float)transformArray.at(13), (float)transformArray.at(14), (float)transformArray.at(15));
 
 
 		//Load hit groups
 		vector<shared_ptr<HitProgram>> hitGroups;
-		auto hitGroupArray = desc["HitGroup"];
+		auto hitGroupArray = desc["HitGroups"];
 
 		for(int i = 0; i < hitGroupArray.size(); i++)
 		{
@@ -30,35 +30,35 @@ void GameObject::LoadFromJson(nlohmann::basic_json<>::value_type& desc, ID3D12Re
 
 		//Get CBuffers
 		vector<ID3D12ResourcePtr> constantBuffers;
-		auto bufferArray = desc["Buffers"];
-		for (int i = 0; i < bufferArray.size(); i++)
+		
+		//Get Metal buffer
+		auto metalDesc = desc["Metal"];
+		if(metalDesc.value("ConstantBufferType", "") == "Metal")
 		{
-			//Get buffer type. Load correct one and get CB ptr;
-			string type = bufferArray[i].value("ConstantBufferType", "");
+			//Load Metal
+			MetalBuffer mb{ metalDesc.value("Shininess", 0.5f),vec3(0),
+				metalDesc.value("Scatter", 0.5f),vec3(0) };
+			constantBuffers.push_back(RendererUtil::CreateConstantBuffer(Renderer::GetInstance()->GetWindowHandle(), Renderer::GetInstance()->GetDevice(), &mb, sizeof(MetalBuffer)));
+		}
 
-			if(type == "Material")
-			{
-				//Load Material
-				MaterialBuffer mat{ vec3(bufferArray[i]["MaterialColour"][0], bufferArray[i]["MaterialColour"][1],bufferArray[i]["MaterialColour"][2]), 0,
-				vec3(bufferArray[i]["SpecularColour"][0], bufferArray[i]["SpecularColour"][1],bufferArray[i]["SpecularColour"][2]), 0, 
-				bufferArray[i].value("SpecularPower", 10) };
+		//Get Material Buffer
+		auto materialDesc = desc["Material"];
+		if (materialDesc.value("ConstantBufferType", "") == "Material")
+		{
+			//Load Material
+			MaterialBuffer mat{ vec3(materialDesc["MaterialColour"][0], materialDesc["MaterialColour"][1],materialDesc["MaterialColour"][2]), 0,
+			vec3(materialDesc["SpecularColour"][0], materialDesc["SpecularColour"][1],materialDesc["SpecularColour"][2]), 0,
+			materialDesc.value("SpecularPower", 10) };
+
+			constantBuffers.push_back(RendererUtil::CreateConstantBuffer(Renderer::GetInstance()->GetWindowHandle(), Renderer::GetInstance()->GetDevice(), &mat, sizeof(MaterialBuffer)));
+		}
 
 
-				constantBuffers.push_back( RendererUtil::CreateConstantBuffer(Renderer::GetInstance()->GetWindowHandle(), Renderer::GetInstance()->GetDevice(), &mat, sizeof(MaterialBuffer)));
-
-			}
-			else if(type == "Metal")
-			{
-				//Load Metal
-				MetalBuffer mb{bufferArray[i].value("Shininess", 0.5f),vec3(0),
-					bufferArray[i].value("Scatter", 0.5f),vec3(0) };
-				constantBuffers.push_back(RendererUtil::CreateConstantBuffer(Renderer::GetInstance()->GetWindowHandle(), Renderer::GetInstance()->GetDevice(), &mb, sizeof(MetalBuffer)));
-			}
-			else if(type == "World")
-			{
-				//Add world cb to buffer array
-				constantBuffers.push_back(worldCB);
-			}
+		//Get Material Buffer
+		auto worldDesc = desc["WorldData"];
+		if (worldDesc.value("ConstantBufferType", "") == "World")
+		{
+			constantBuffers.push_back(worldCB);
 		}
 
 		Instance inst{ transform, hitGroups, constantBuffers };
