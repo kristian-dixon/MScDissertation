494dc7f002caf4ea3f6bf3e559bfe5ce9c397fbd
Kristian <kristiandixon1206@gmail.com>
2019-11-26 11:07:02 +0000

    Added gamepad support to IMGUI

--- RTX_Dissertation/RTX_Dissertation/IMGUI_Implementation.cpp
+++ RTX_Dissertation/RTX_Dissertation/IMGUI_Implementation.cpp
@@ -32,7 +32,7 @@ void IMGUI_Implementation::CreateIMGUIWindow(HWND hwnd)
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
